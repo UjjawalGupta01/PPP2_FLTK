ad9554b18ded1c072346be48ea9cc3b9b1a785ec
Mike Benton <bikepunk005@hotmail.com>
2021-08-13 20:50:52 -0400

    Update formats

--- src/Simple_window.h
+++ src/Simple_window.h
@@ -1,43 +1,67 @@
 #pragma once
 
-#include <string>
-#include <FL/Fl.H>
-
-#include "Point.h"
 #include "Gui.h"
+#include "Point.h"
 
-// Simple_window is basic scaffolding for ultra-simple interaction with graphics
-// it provides one window with one "next" button for ultra-simple animation
+#include <FL/Fl.H>
+#include <string>
+
+// Simple_window is basic scaffolding for ultra-simple interaction with
+// graphics it provides one window with one "next" button for ultra-simple
+// animation
 namespace Graph_lib {
-	struct Simple_window : Window {
-		Simple_window(Point xy, int w, int h, const std::string& title)
-			: Window(xy, w, h, title),
-			button_pushed(false),
-			next_button(Point(x_max() - 70, 0), 70, 20, "Next", cb_next) {
-			attach(next_button);
-		}
-
-		void wait_for_button()
-			// modified event loop
-			// handle all events (as per default), but quit when button_pushed becomes true
-			// this allows graphics without control inversion
-		{
-			while (!button_pushed) Fl::wait();
-			button_pushed = false;
-			Fl::redraw();
-		}
-
-		Button next_button;
-	private:
-		bool button_pushed;
-
-		static void cb_next(Address, Address addr) // callback for next_button
-		//	{ reference_to<Simple_window>(addr).next(); }
-		{
-			static_cast<Simple_window*>(addr)->next();
-		}
-
-		void next() { button_pushed = true; }
-
-	};
+void test(auto foo)
+{
+    switch (foo) {
+    case 1: {
+        bar();
+        break;
+    }
+    default: {
+        plop();
+    }
+    }
 }
+constexpr auto next_width {70};    // test1
+constexpr auto next_bottom {20};    // test2
+
+class Simple_window : public Window
+{
+    public:
+    Simple_window(Point xy, int w, int h, const std::string& title):
+        Window(xy, w, h, title), button_pushed(false),
+        next_button(Point(x_max() - next_width, 0), next_width, next_bottom,
+                    "Next", cb_next)
+    {
+        attach(next_button);
+    }
+
+    void wait_for_button()
+    // modified event loop
+    // handle all events (as per default), but quit when button_pushed becomes
+    // true this allows graphics without control inversion
+    {
+        while (!button_pushed) {
+            Fl::wait();
+        }
+        button_pushed = false;
+        Fl::redraw();
+    }
+
+    Button next_button;
+
+    private:
+    bool button_pushed;
+
+    static void cb_next(Address, Address addr)    // callback for next_button
+    //  { reference_to<Simple_window>(addr).next(); }
+    {
+        static_cast<Simple_window*>(addr)->next();
+    }
+
+    void next()
+    {
+        button_pushed = true;
+    }
+};
+}    // namespace Graph_lib
--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 
-#include "Simple_window.h"
 #include "Graph.h"
+#include "Simple_window.h"
 
 int main()
 {
-    const Graph_lib::Point wind_start {100,100};
+    const Graph_lib::Point wind_start {100, 100};
     constexpr auto win_size {1200};
     const auto smile_size {wind_start.x};
 
+    Graph_lib::Simple_window win1 {wind_start, win_size, win_size,
+                                   "Test of Circle"};
 
-    Graph_lib::Simple_window win1  {wind_start,win_size,win_size,"Test of Circle"};
-
-    Graph_lib::Circle cir {wind_start,smile_size};
+    Graph_lib::Circle cir {wind_start, smile_size};
     win1.attach(cir);
 
     win1.wait_for_button();
